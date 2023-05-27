3d08fe4aa2b1dbd99c6d52738bda6ad857ef90dd
Chloe Chau <77cchau@gmail.com>
2023-05-27 11:15:07 -0700

    navbar structure and placeholder links

--- rateuci-frontend/src/pages/home/home.js
+++ rateuci-frontend/src/pages/home/home.js
@@ -1,8 +1,10 @@
 import Card from '../../components/card/card.js'
+import NavBar from '../../components/navbar/navbar.js';
 
 function Home() {
     return (
         <div>
+            <NavBar />
             <Card message='Hello World!'/>
         </div>);
 }
