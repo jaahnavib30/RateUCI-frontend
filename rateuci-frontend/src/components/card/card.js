608909e51f26400cf4316277c8f4b95729fd4ae5
Chloe Chau <77cchau@gmail.com>
2023-05-27 10:35:53 -0700

    add skeleton for card component

--- rateuci-frontend/src/components/card/card.js
+++ rateuci-frontend/src/components/card/card.js
@@ -1,7 +1,14 @@
-
+import "./card.css"
 
 function Card(props) {
-    return (<div className='card-container'>{props.message}</div>);
+    return (
+        <div className='card-container'>
+            <h2>{props.event_name}</h2>
+            <ul>
+                <li>{props.event_date}</li>
+                <li>{props.event_location}</li>
+            </ul>
+        </div>);
 }
 
 export default Card;
--- rateuci-frontend/src/pages/home/home.js
+++ rateuci-frontend/src/pages/home/home.js
@@ -3,7 +3,9 @@ import Card from '../../components/card/card.js'
 function Home() {
     return (
         <div>
-            <Card message='Hello World!'/>
+            <Card event_name='Hello World!' 
+                  event_date='insert date here' 
+                  event_location='insert location here'/>
         </div>);
 }
 
